ee81abf32cbe5f8e4c499ebece8146123552932d
Alaa Mroue <alaa.baghdadi_mroue@stud.h2.de>
2023-06-21 10:17:10 +0200

    PromaidesCLC: dDischarge with A-flow

--- src/CSchemePromaides.cpp
+++ src/CSchemePromaides.cpp
@@ -211,7 +211,7 @@ bool CSchemePromaides::preparePromaidesKernels()
 	// --
 
 
-	oclKernelFullTimestep = oclModel->getKernel( "pro_cacheDisabled " );
+	oclKernelFullTimestep = oclModel->getKernel( "pro_cacheDisabled" );
 	oclKernelFullTimestep->setGroupSize( this->ulNonCachedWorkgroupSizeX, this->ulNonCachedWorkgroupSizeY );
 	oclKernelFullTimestep->setGlobalSize( this->ulNonCachedGlobalSizeX, this->ulNonCachedGlobalSizeY );
 	COCLBuffer* aryArgsFullTimestep[] = { oclBufferTimestep, oclBufferCellBed, oclBufferCellStates, oclBufferCellStatesAlt, oclBufferCellManning };	
