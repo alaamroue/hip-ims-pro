0c11580de13f19b79a9f7f59d0dea544f5ccc99d
Alaa Mroue <alaa.baghdadi_mroue@stud.h2.de>
2023-08-13 19:11:32 +0200

    Added Poleni to Promaides Scheme

--- src/CSchemePromaides.cpp
+++ src/CSchemePromaides.cpp
@@ -214,7 +214,7 @@ bool CSchemePromaides::preparePromaidesKernels()
 	oclKernelFullTimestep = oclModel->getKernel( "pro_cacheDisabled" );
 	oclKernelFullTimestep->setGroupSize( this->ulNonCachedWorkgroupSizeX, this->ulNonCachedWorkgroupSizeY );
 	oclKernelFullTimestep->setGlobalSize( this->ulNonCachedGlobalSizeX, this->ulNonCachedGlobalSizeY );
-	COCLBuffer* aryArgsFullTimestep[] = { oclBufferTimestep, oclBufferCellBed, oclBufferCellStates, oclBufferCellStatesAlt, oclBufferCellManning, oclBufferUsePoleni };
+	COCLBuffer* aryArgsFullTimestep[] = { oclBufferTimestep, oclBufferCellBed, oclBufferCellStates, oclBufferCellStatesAlt, oclBufferCellManning, oclBufferUsePoleni, oclBuffer_opt_zxmax, oclBuffer_opt_cx, oclBuffer_opt_zymax, oclBuffer_opt_cy };
 	oclKernelFullTimestep->assignArguments( aryArgsFullTimestep );
 
 
